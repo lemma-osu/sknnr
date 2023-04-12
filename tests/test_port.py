7596bd3b15b9484140b6e3302ef224e212f0fd48
Aaron Zuspan <aa.zuspan@gmail.com>
2023-04-12 14:13:54 -0700

    Remove redundant and unused Dataset attrs

--- tests/test_port.py
+++ tests/test_port.py
@@ -8,23 +8,17 @@ import pytest
 
 class Dataset:
     def __init__(self, project, method, k=5):
-        self.project = project
-        self.method = method
-        self.k = k
-        
-        self.distances = pd.read_csv(f"./tests/data/{method}_{project}_ref_distances_k{k}.csv")
-        self.neighbors = pd.read_csv(f"./tests/data/{method}_{project}_ref_neighbors_k{k}.csv")
-        self.env_df = pd.read_csv(f"./tests/data/{project}_env.csv")
-        self.spp_df = pd.read_csv(f"./tests/data/{project}_spp.csv")
-
+        distances_df = pd.read_csv(f"./tests/data/{method}_{project}_ref_distances_k{k}.csv")
+        neighbors_df = pd.read_csv(f"./tests/data/{method}_{project}_ref_neighbors_k{k}.csv")
+        env_df = pd.read_csv(f"./tests/data/{project}_env.csv")
+        spp_df = pd.read_csv(f"./tests/data/{project}_spp.csv")
         cols = [f"K{i+1}" for i in range(k)]
 
-        self.distances = self.distances.loc[:, cols].values
-        self.neighbors = self.neighbors.loc[:, cols].values
-        self.X = self.env_df.iloc[:, 1:].values
-        self.Y = self.spp_df.iloc[:, 1:].values
-        self.ids = self.env_df.iloc[:, 0].values
-        self.spp = self.spp_df.iloc[:, 1:].values
+        self.distances = distances_df.loc[:, cols].values
+        self.neighbors = neighbors_df.loc[:, cols].values
+        self.X = env_df.iloc[:, 1:].values
+        self.y = spp_df.iloc[:, 1:].values
+        self.ids = env_df.iloc[:, 0].values
 
 
 @pytest.fixture
@@ -61,7 +55,7 @@ def test_moscow_euc(moscow_euc):
 
 
 def test_moscow_gnn(moscow_gnn):
-    clf = GNN(n_neighbors=5).fit(moscow_gnn.X, moscow_gnn.ids, spp=moscow_gnn.Y)
+    clf = GNN(n_neighbors=5).fit(moscow_gnn.X, moscow_gnn.ids, spp=moscow_gnn.y)
     dist, _ = clf.kneighbors()
     nn = clf.kneighbor_ids()
 
