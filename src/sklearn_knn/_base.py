63017f0a9fa55b121cd5b077f61ad9edace53a71
Matt Gregory <matt.gregory@oregonstate.edu>
2023-04-14 12:54:16 -0700

    WIP: Add fitted attribute to store transformation
    - Add kneighbors and kneighbor_ids methods to ensure transformation is
      applied
    - Start on work for TransformedKNeighborsMixin (commented)

--- src/sklearn_knn/_base.py
+++ src/sklearn_knn/_base.py
@@ -19,3 +19,15 @@ class MyStandardScaler(StandardScaler):
         sc = super().fit(X, y, sample_weight)
         sc.scale_ = np.std(X, axis=0, ddof=1)
         return sc
+
+
+# from sklearn.utils.validation import check_is_fitted
+#
+# class TransformedKNeighborsMixin(KNeighborsClassifier):
+#     def kneighbors(self, X=None, n_neighbors=None, return_distance=True):
+#         if X is not None:
+#             check_is_fitted(self, "transform_")
+#             X = self.transform_.transform(X)
+#         return super().kneighbors(
+#             X=X, n_neighbors=n_neighbors, return_distance=return_distance
+#         )
--- src/sklearn_knn/_euclidean.py
+++ src/sklearn_knn/_euclidean.py
@@ -1,11 +1,28 @@
+from sklearn.utils.validation import check_is_fitted
+
 from ._base import MyStandardScaler, IDNeighborsClassifier
 
 
 class Euclidean(IDNeighborsClassifier):
     def fit(self, X, y):
-        X = MyStandardScaler().fit_transform(X)
+        self.transform_ = MyStandardScaler().fit(X, y)
+        X = self.transform_.transform(X)
         return super().fit(X, y)
-    
+
     def predict(self, X):
-        X = MyStandardScaler().fit_transform(X)
+        X = self.transform_.transform(X)
         return super().predict(X)
+
+    def kneighbor_ids(self, X=None, n_neighbors=None):
+        if X is not None:
+            check_is_fitted(self, "transform_")
+            X = self.transform_.transform(X)
+        return super().kneighbor_ids(X=X, n_neighbors=n_neighbors)
+
+    def kneighbors(self, X=None, n_neighbors=None, return_distance=True):
+        if X is not None:
+            check_is_fitted(self, "transform_")
+            X = self.transform_.transform(X)
+        return super().kneighbors(
+            X=X, n_neighbors=n_neighbors, return_distance=return_distance
+        )
