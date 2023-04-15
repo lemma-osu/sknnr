3e2c47f2bd67c6978af45c7de227d7242fbaf04c
Aaron Zuspan <aa.zuspan@gmail.com>
2023-04-14 17:07:45 -0700

    Move test datasets into conftest to make them available to other test modules

--- tests/test_port.py
+++ tests/test_port.py
@@ -1,52 +1,7 @@
 from numpy.testing import assert_array_equal, assert_array_almost_equal
 from sklearn.model_selection import train_test_split
-import pandas as pd
 
 from sklearn_knn import Raw, Euclidean, GNN
-
-import pytest
-
-
-class Dataset:
-    def __init__(self, project, method, k=5):
-        ref_distances_df = pd.read_csv(
-            f"./tests/data/{method}_{project}_ref_distances_k{k}.csv"
-        )
-        ref_neighbors_df = pd.read_csv(
-            f"./tests/data/{method}_{project}_ref_neighbors_k{k}.csv"
-        )
-        trg_distances_df = pd.read_csv(
-            f"./tests/data/{method}_{project}_trg_distances_k{k}.csv"
-        )
-        trg_neighbors_df = pd.read_csv(
-            f"./tests/data/{method}_{project}_trg_neighbors_k{k}.csv"
-        )
-        env_df = pd.read_csv(f"./tests/data/{project}_env.csv")
-        spp_df = pd.read_csv(f"./tests/data/{project}_spp.csv")
-        cols = [f"K{i+1}" for i in range(k)]
-
-        self.ref_distances = ref_distances_df.loc[:, cols].values
-        self.ref_neighbors = ref_neighbors_df.loc[:, cols].values
-        self.trg_distances = trg_distances_df.loc[:, cols].values
-        self.trg_neighbors = trg_neighbors_df.loc[:, cols].values
-        self.X = env_df.iloc[:, 1:].values
-        self.y = spp_df.iloc[:, 1:].values
-        self.ids = env_df.iloc[:, 0].values
-
-
-@pytest.fixture
-def moscow_raw():
-    return Dataset(project="moscow", method="raw", k=5)
-
-
-@pytest.fixture
-def moscow_euc():
-    return Dataset(project="moscow", method="euc", k=5)
-
-
-@pytest.fixture
-def moscow_gnn():
-    return Dataset(project="moscow", method="gnn", k=5)
 
 
 # def test_moscow_raw(moscow_raw):
