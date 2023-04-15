835a78930626a8b866e5edf8a7280b6236043f7e
Aaron Zuspan <aa.zuspan@gmail.com>
2023-04-14 17:12:44 -0700

    Implement TransformedKNeighborsMixin
    Estimators that use a transformer need to run transform when kneighbors
    is called with new data. The TransformedKNeighborsMixin overrides
    kneighbors to run that transform. This mixin may be able to handle
    running the transform when fitting and predicting with those
    estimators, but that would require handling custom parameters like
    CCA parameters for GNN, and may make function signatures less
    readable. Worth considering further at a later date.

--- src/sklearn_knn/_base.py
+++ src/sklearn_knn/_base.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
+from sklearn.utils.validation import check_is_fitted
 
 
 class IDNeighborsClassifier(KNeighborsClassifier):
@@ -14,20 +15,22 @@ class IDNeighborsClassifier(KNeighborsClassifier):
         return self.classes_[self._y[neigh_ind]]
 
 
+class TransformedKNeighborsMixin(KNeighborsClassifier):
+    """
+    Mixin for KNeighbors classifiers that store a `transform_` during fitting 
+    (e.g. GNN).
+    """
+    def kneighbors(self, X=None, n_neighbors=None, return_distance=True):
+        if X is not None:
+            check_is_fitted(self, "transform_")
+            X = self.transform_.transform(X)
+        return super().kneighbors(
+            X=X, n_neighbors=n_neighbors, return_distance=return_distance
+        )
+    
+
 class MyStandardScaler(StandardScaler):
     def fit(self, X, y=None, sample_weight=None):
         sc = super().fit(X, y, sample_weight)
         sc.scale_ = np.std(X, axis=0, ddof=1)
         return sc
-
-
-# from sklearn.utils.validation import check_is_fitted
-#
-# class TransformedKNeighborsMixin(KNeighborsClassifier):
-#     def kneighbors(self, X=None, n_neighbors=None, return_distance=True):
-#         if X is not None:
-#             check_is_fitted(self, "transform_")
-#             X = self.transform_.transform(X)
-#         return super().kneighbors(
-#             X=X, n_neighbors=n_neighbors, return_distance=return_distance
-#         )
--- src/sklearn_knn/_euclidean.py
+++ src/sklearn_knn/_euclidean.py
@@ -1,28 +1,14 @@
 from sklearn.utils.validation import check_is_fitted
+from ._base import MyStandardScaler, IDNeighborsClassifier, TransformedKNeighborsMixin
 
-from ._base import MyStandardScaler, IDNeighborsClassifier
 
-
-class Euclidean(IDNeighborsClassifier):
+class Euclidean(IDNeighborsClassifier, TransformedKNeighborsMixin):
     def fit(self, X, y):
         self.transform_ = MyStandardScaler().fit(X, y)
         X = self.transform_.transform(X)
         return super().fit(X, y)
 
     def predict(self, X):
+        check_is_fitted(self)
         X = self.transform_.transform(X)
         return super().predict(X)
-
-    def kneighbor_ids(self, X=None, n_neighbors=None):
-        if X is not None:
-            check_is_fitted(self, "transform_")
-            X = self.transform_.transform(X)
-        return super().kneighbor_ids(X=X, n_neighbors=n_neighbors)
-
-    def kneighbors(self, X=None, n_neighbors=None, return_distance=True):
-        if X is not None:
-            check_is_fitted(self, "transform_")
-            X = self.transform_.transform(X)
-        return super().kneighbors(
-            X=X, n_neighbors=n_neighbors, return_distance=return_distance
-        )
