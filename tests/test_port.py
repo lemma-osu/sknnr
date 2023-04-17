14c32091b216999e2b5292b9e146b5a11fad6082
Aaron Zuspan <aa.zuspan@gmail.com>
2023-04-17 10:38:15 -0700

    Remove unused port tests

--- tests/test_port.py
+++ tests/test_port.py
@@ -4,33 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn_knn import Raw, Euclidean, GNN
 
 
-# def test_moscow_raw(moscow_raw):
-#     clf = Raw(n_neighbors=5).fit(moscow_raw.X, moscow_raw.ids)
-#     dist, _ = clf.kneighbors()
-#     nn = clf.kneighbor_ids()
-
-#     assert_array_equal(nn, moscow_raw.neighbors)
-#     assert_array_almost_equal(dist, moscow_raw.distances, decimal=3)
-
-
-# def test_moscow_euc(moscow_euc):
-#     clf = Euclidean(n_neighbors=5).fit(moscow_euc.X, moscow_euc.ids)
-#     dist, _ = clf.kneighbors()
-#     nn = clf.kneighbor_ids()
-
-#     assert_array_equal(nn, moscow_euc.neighbors)
-#     assert_array_almost_equal(dist, moscow_euc.distances, decimal=3)
-
-
-# def test_moscow_gnn(moscow_gnn):
-#     clf = GNN(n_neighbors=5).fit(moscow_gnn.X, moscow_gnn.ids, spp=moscow_gnn.y)
-#     dist, _ = clf.kneighbors()
-#     nn = clf.kneighbor_ids()
-
-#     assert_array_equal(nn, moscow_gnn.neighbors)
-#     assert_array_almost_equal(dist, moscow_gnn.distances, decimal=3)
-
-
 def test_moscow_raw(moscow_raw):
     X_train, X_test, y_train, _ = train_test_split(
         moscow_raw.X, moscow_raw.ids, train_size=0.8, shuffle=False
