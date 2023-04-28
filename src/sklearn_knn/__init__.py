11aad97fa887c8cc7b110bcc57aef90b1997f1b2
Aaron Zuspan <aa.zuspan@gmail.com>
2023-04-28 09:46:32 -0700

    Update authors and move __version__ into __about__.py

--- src/sklearn_knn/__init__.py
+++ src/sklearn_knn/__init__.py
@@ -1,10 +1,9 @@
+from .__about__ import __version__  # noqa: F401
 from ._euclidean import Euclidean
 from ._gnn import GNN
 from ._mahalanobis import Mahalanobis
 from ._msn import MSN
 from ._raw import Raw
-
-__version__ = "0.1.0"
 
 __all__ = [
     "Raw",
