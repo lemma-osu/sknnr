d2c6aacf2f2072fd821c48baed2ea17b6a32c592
Aaron Zuspan <50475791+aazuspan@users.noreply.github.com>
2023-05-15 14:00:33 -0700

    Store feature names for transformed estimators #20
    Transformed estimators like GNNRegressor run a transformer on X
    before fitting or predicting. When X is a dataframe, transforming
    converts it into an array, preventing sklearn from extracting
    feature names. To fix this, we wrap the transformed array in an
    ndarray subclass called NamedFeatureArray that is capable of
    storing a `columns` attribute prior to passing it to `fit` or
    `predict`. This tricks sklearn into thinking that it is a
    dataframe and allows feature names to be successfully accessed
    and set on the estimator.
    
    To accomplish this cleanly, we move all the actual transformation
    steps out of the individual estimators and in to the
    TransformedKNeighborsMixin methods. If we need to implement
    different `predict` methods for different estimators in the
    future, they can be re-implemented at the estimator level to use
    the _transform method of their superclass.
    
    To prevent regressions, this commit also expands the dataframe
    support test to check feature names are correctly stored.

--- src/sknnr/_base.py
+++ src/sknnr/_base.py
@@ -1,5 +1,24 @@
+import numpy as np
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.utils.validation import check_is_fitted
+
+
+class NamedFeatureArray(np.ndarray):
+    """An array with a columns attribute indicating feature names.
+
+    Storing a `columns` attribute allows this array to act like  a dataframe for the
+    purpose of extracting feature names when passed to sklearn estimators.
+    """
+
+    def __new__(cls, array, columns=None):
+        obj = np.asarray(array).view(cls)
+        obj.columns = columns
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is None:
+            return
+        self.columns = getattr(obj, "columns", None)
 
 
 class IDNeighborsRegressor(KNeighborsRegressor):
@@ -10,14 +29,39 @@ class IDNeighborsRegressor(KNeighborsRegressor):
 
 class TransformedKNeighborsMixin(KNeighborsRegressor):
     """
-    Mixin for KNeighbors regressors that store a `transform_` during fitting (e.g.
-    GNN).
+    Mixin for KNeighbors regressors that apply transformations to the feature data.
     """
 
+    def _apply_transform(self, X) -> NamedFeatureArray:
+        """Apply the stored transform to the input data.
+
+        Note
+        ----
+        Transforming will cast input data to numpy arrays. To preserve feature names
+        in the case of dataframe inputs, this method will wrap the transformed array
+        in a `NamedFeatureArray` with a `columns` attribute, allowing `sklearn` to
+        parse and store feature names.
+        """
+        check_is_fitted(self, "transform_")
+        X_transformed = self.transform_.transform(X)
+        if hasattr(X, "columns"):
+            X_transformed = NamedFeatureArray(X_transformed, columns=X.columns)
+
+        return X_transformed
+
+    def fit(self, X, y):
+        """Fit using transformed feature data."""
+        X_transformed = self._apply_transform(X)
+        return super().fit(X_transformed, y)
+
+    def predict(self, X):
+        """Predict using transformed feature data."""
+        X_transformed = self._apply_transform(X)
+        return super().predict(X_transformed)
+
     def kneighbors(self, X=None, n_neighbors=None, return_distance=True):
-        if X is not None:
-            check_is_fitted(self, "transform_")
-            X = self.transform_.transform(X)
+        """Return neighbor indices and distances using transformed feature data."""
+        X_transformed = self._apply_transform(X) if X is not None else X
         return super().kneighbors(
-            X=X, n_neighbors=n_neighbors, return_distance=return_distance
+            X=X_transformed, n_neighbors=n_neighbors, return_distance=return_distance
         )
