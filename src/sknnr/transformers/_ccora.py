f9fc703328b6aaaaabed8d33b730bc6b35ac018b
Matt Gregory <matt.gregory@oregonstate.edu>
2023-05-26 15:02:21 -0700

    Add reference markers for derived and licensed external code

--- src/sknnr/transformers/_ccora.py
+++ src/sknnr/transformers/_ccora.py
@@ -3,7 +3,9 @@ from scipy.stats import f
 
 
 def ftest_cor(p, q, N, cor):
-    """ "Direct port of yaImpute ftest_cor in yai"""
+    """Direct port of yaImpute ftest_cor in yai
+    REFERENCE: https://github.com/jeffreyevans/yaImpute/blob/f06d1daf13cf461730c3da5f18f022c1074e9826/R/yai.R#L11-L42 # noqa: E501
+    """
     s = min(p, q)
     k = np.arange(s) + 1
     L = np.array([np.prod(1 - np.square(cor[i:s])) for i in range(s)])
@@ -40,6 +42,17 @@ def ftest_cor(p, q, N, cor):
 
 
 class CCorA:
+    """This class closely follows the implementation of
+    `statsmodels.multivariate.cancorr.CanCorr` for finding the coefficients associated
+    with canonical correlation. The exception to this is the `cscal` property which is
+    used to standardize the coefficients and comes from yaImpute.
+
+    We were not able to use the statsmodels implementation directly as it detects
+    collinear features and raises a `ValueError` which yaImpute allows to pass silently.
+    REFERENCE: https://github.com/statsmodels/statsmodels/blob/77cb066320391ffed4196a32491ddca28e8c9122/statsmodels/multivariate/cancorr.py#L17-L93 # noqa: E501
+    REFERENCE: https://github.com/jeffreyevans/yaImpute/blob/f06d1daf13cf461730c3da5f18f022c1074e9826/R/yai.R#L367-L371 # noqa: E501
+    """
+
     TOLERANCE = 1e-8
     P_VAL = 0.05
 
