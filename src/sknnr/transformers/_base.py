lemma-osu/sknnr

commit ae58e601e002a6245f66327c99340d4cb7e999d8

Fix StandardScalerWithDOF()-check_dtype_object
This test failed because "object" types that *could* be cast to
floats were not being cast. To fix that, I copied the _validate_data
call from StandardScaler.

Note that I also copied the sparse and finite options, since we're
not doing anything in the transformer that would preclude them.

src/sknnr/transformers/_base.py
@@ -3,7 +3,7 @@ from typing import Union
 import numpy as np
 from numpy.typing import NDArray
 from sklearn.preprocessing import StandardScaler
-from sklearn.utils.validation import check_is_fitted
+from sklearn.utils.validation import FLOAT_DTYPES, check_is_fitted
 
 from ._cca import CCA
 from ._ccora import CCorA
@@ -17,6 +17,14 @@ class StandardScalerWithDOF(StandardScaler):
         self.ddof = ddof
 
     def fit(self, X, y=None, sample_weight=None):
+        X = self._validate_data(
+            X,
+            accept_sparse=("csr", "csc"),
+            dtype=FLOAT_DTYPES,
+            force_all_finite="allow-nan",
+            reset=True,
+        )
+
         scaler = super().fit(X, y, sample_weight)
         scaler.scale_ = np.std(np.asarray(X), axis=0, ddof=self.ddof)
         return scaler

commit 9c2fec9b017c422c33bdead129f1d5a562d84064

Fix bug introduced by ae58e60
We need to use _validate_data after fitting the superclass to avoid
losing feature names for dataframe inputs. Note that this also set
reset=False since some fit attributes will be set when we fit the
superclass, and we can remove the np.asarray(X) cast since that's
now handled by _validate_data.

src/sknnr/transformers/_base.py
@@ -17,16 +17,16 @@ class StandardScalerWithDOF(StandardScaler):
         self.ddof = ddof
 
     def fit(self, X, y=None, sample_weight=None):
+        scaler = super().fit(X, y, sample_weight)
+
         X = self._validate_data(
             X,
             accept_sparse=("csr", "csc"),
             dtype=FLOAT_DTYPES,
             force_all_finite="allow-nan",
-            reset=True,
+            reset=False,
         )
-
-        scaler = super().fit(X, y, sample_weight)
-        scaler.scale_ = np.std(np.asarray(X), axis=0, ddof=self.ddof)
+        scaler.scale_ = np.std(X, axis=0, ddof=self.ddof)
         return scaler
 
 