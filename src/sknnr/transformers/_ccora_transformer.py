87747de75220d05a074767692616a58cd51d0cf8
Aaron Zuspan <aa.zuspan@gmail.com>
2023-08-22 15:19:56 -0700

    Fix CCorATransformer() checks by reshaping 1D y arrays
    1D y arrays caused StandardScalerWithDOF.fit_transform to fail
    due to an inherited check with `ensure_2d=True`. Now, 1D y arrays
    are reshaped to 2D before scaling, following the pattern of
    `sklearn.cross_decomposition.CCA`. This fixed a large number of
    failing estimator checks for the transformer.
    -

--- src/sknnr/transformers/_ccora_transformer.py
+++ src/sknnr/transformers/_ccora_transformer.py
@@ -1,5 +1,6 @@
+import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.utils.validation import check_is_fitted
+from sklearn.utils.validation import check_array, check_is_fitted
 
 from . import ComponentReducerMixin, StandardScalerWithDOF
 from ._ccora import CCorA
@@ -8,9 +9,13 @@ from ._ccora import CCorA
 class CCorATransformer(ComponentReducerMixin, TransformerMixin, BaseEstimator):
     def fit(self, X, y):
         self._validate_data(X, reset=True)
-
         self.scaler_ = StandardScalerWithDOF(ddof=1).fit(X)
+
+        y = check_array(y, input_name="Y", ensure_2d=False, dtype=np.float64)
+        if y.ndim == 1:
+            y = y.reshape(-1, 1)
         y = StandardScalerWithDOF(ddof=1).fit_transform(y)
+
         self.ordination_ = CCorA(self.scaler_.transform(X), y)
         self.set_n_components()
         return self
