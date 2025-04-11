28394f00c5cbe2c634255dda4dc273b6df777ce0
Matt Gregory <matt.gregory@oregonstate.edu>
2025-04-11 16:23:31 -0700

    Rename KNNTestDataset to TrainTestDataset

--- tests/datasets.py
+++ tests/datasets.py
@@ -10,14 +10,14 @@ from sknnr.datasets import load_moscow_stjoes
 
 
 @dataclass
-class KNNTestDataset:
+class TrainTestDataset:
     X_train: NDArray[np.float64]
     X_test: NDArray[np.float64]
     y_train: NDArray[np.float64]
     y_test: NDArray[np.float64]
 
 
-def load_moscow_stjoes_test_data() -> KNNTestDataset:
+def load_moscow_stjoes_test_data() -> TrainTestDataset:
     """Load the Moscow Mountain / St. Joe's dataset for regression testing.
 
     The dataset will always be in dataframe format, with a fixed 80%/20% train/test
@@ -28,6 +28,6 @@ def load_moscow_stjoes_test_data() -> KNNTestDataset:
         X, y, train_size=0.8, shuffle=False
     )
 
-    return KNNTestDataset(
+    return TrainTestDataset(
         X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test
     )
